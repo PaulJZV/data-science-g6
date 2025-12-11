2dd7ddba7c50897358d12f9d276d3144bbb9f975
Paul Junior <paulvargas0894@gmail.com>
2025-12-11 11:22:24 -0500

    datos

---
+++ Dia3/2-tiposdatos.py
@@ -0,0 +1,15 @@
+# CREAMOS UN PROGRAMA QUE ME SUME 2 NÚMEROS
+# PARA CREAR UN PROGRAMA DIVIDIMOS EL CODIGO EN 3 PARTES
+# 1. DATOS DE ENTRADA
+# 2. PROCESAMIENTO DE DATOS
+# 3. DATOS DE SALIDA
+
+# DATOS DE ENTRADA
+numero1 = int(input('Ingrese el primero numero: '))
+numero2 = int(input('Ingrese el segundo numero: '))
+
+# PROCESO
+suma = numero1 + numero2
+
+# DATOS DE SALIDA
+print("La suma de los numeros es: ", suma)
